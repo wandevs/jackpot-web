5d42368cfd708f720a47f057ac82fcf72fe9fadb
ZaneCui <897140741@qq.com>
2020-05-25 18:30:04 +0800

    Show draw results when the account is not selected

--- conf/config.js
+++ conf/config.js
@@ -1,6 +1,7 @@
 
 export const mainnetSCAddr = '';//mainnet 8 hours smart contract
 export const testnetSCAddr = '0x9c6F5b86595DA99a769217af38B6599ebA1806EF';//testnet 8 hours smart contract
+// export const testnetSCAddr = '0xd8b67e4c66a4ab51fa8b7a30a82c63ff792b79c0';//testnet 8 hours smart contract
 
 // change networkId to switch network
 export const networkId = 3; //1:mainnet, 3:testnet;
@@ -8,6 +9,7 @@ export const networkId = 3; //1:mainnet, 3:testnet;
 // export const nodeUrl = networkId == 1 ? "https://gwan-ssl.wandevs.org:56891" : "http://192.168.1.179:54320";
 // export const nodeUrl = networkId == 1 ? "https://gwan-ssl.wandevs.org:56891" : "https://molin.tech:16666";
 export const nodeUrl = networkId == 1 ? "https://gwan-ssl.wandevs.org:56891" : "http://192.168.1.89:8545";
+// export const nodeUrl = networkId == 1 ? "https://gwan-ssl.wandevs.org:56891" : "https://gwan-ssl.wandevs.org:46891";
 
 // export const nodeUrl = networkId == 1 ? "https://mywanwallet.io/api" : "https://demodex.wandevs.org:48545";
 
