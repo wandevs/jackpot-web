7af4a42a7f13156b92eddb67d6ffa0c99c8f4dbb
lolieatapple <lolieatapple@gmail.com>
2020-06-09 12:32:33 +0800

    update mainnet sc addr.

--- conf/config.js
+++ conf/config.js
@@ -1,9 +1,9 @@
-export const mainnetSCAddr = '';//mainnet 8 hours smart contract
+export const mainnetSCAddr = '0x76b074d91f546914c6765ef81cbdc6f9c7da5685';//mainnet 8 hours smart contract
 export const testnetSCAddr = '0xd8b67e4c66a4ab51fa8b7a30a82c63ff792b79c0';//testnet 8 hours smart contract
 // export const testnetSCAddr = '0xd8b67e4c66a4ab51fa8b7a30a82c63ff792b79c0';//testnet 8 hours smart contract
 
 // change networkId to switch network
-export const networkId = 3; //1:mainnet, 3:testnet;
+export const networkId = 1; //1:mainnet, 3:testnet;
 
 export const price = 10;
 
