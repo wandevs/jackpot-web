e48dee1228ae9c015bf1d8a1d774f039e9093109
lolieatapple <lolieatapple@gmail.com>
2020-06-26 12:03:21 +0800

    update insight

--- layouts/index.js
+++ layouts/index.js
@@ -320,6 +320,7 @@ class Layout extends Component {
               <li><span className={style['text']}>If there is no winner, the prize pot will automatically accumulate to the next cycle.</span></li>
               <li><span className={style['text']}>If you do not withdraw your tickets, those tickets will automatically participate in the next cycle with your chosen numbers.</span></li>
               <li><span className={style['text']}>Smart Contract Open Source:&nbsp;&nbsp;<a onClick={this.openSC} href="#">https://github.com/wandevs/jackpot-smart-contracts</a></span></li>
+              <li><span className={style['text']}>Jack's Pot Insight:&nbsp;&nbsp;<a onClick={this.openSC} href="#">https://jacks-pot-chart.now.sh/</a></span></li>
             </ul>
           </div>
         </div>
